d2d79c761c8798b32bf951252bd6afbbfda5456a
Adrian Furman <fadikk367@gmail.com>
2020-07-15 17:20:27 +0200

    Do a backup of starting point for further refactors

--- csv/src/index.ts
+++ csv/src/index.ts
@@ -5,7 +5,6 @@ const reader = new CsvFileReader('football.csv');
 reader.read();
 
 
-
 let manUnitedWins = 0;
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HOME_WIN) manUnitedWins++;
