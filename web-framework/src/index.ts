b2654d4cc5457240e343fe321e73b828dce66c47
Adrian Furman <fadikk367@gmail.com>
2020-07-28 23:53:35 +0200

    Extract fetch & save methods to separate class - Sync

--- web-framework/src/index.ts
+++ web-framework/src/index.ts
@@ -1,1 +1,12 @@
 import { User } from './models/User';
+
+const user = new User({ name: 'new record', age: 0 });
+
+user.events.on('change', () => {
+    console.log('changeeee');
+});
+
+user.events.trigger('change');
+
+
+console.log(user);
--- web-framework/src/models/User.ts
+++ web-framework/src/models/User.ts
@@ -1,12 +1,18 @@
-import axios, { AxiosResponse } from 'axios';
+import { Event } from './Event'; 
+import { Sync } from './Sync';
 
-interface UserProps {
+export interface UserProps {
   id?: number,
   name?: string,
   age?: number
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
+  public events: Event = new Event();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+
   constructor(private data: UserProps) {}
 
   get(propName: string): (string | number) {
@@ -17,19 +23,7 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
 
-  save(): void {
-    const id = this.get('id') as number;
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      axios.post(`http://localhost:3000/users`, this.data);
-    }
-  }
+
+
 }
